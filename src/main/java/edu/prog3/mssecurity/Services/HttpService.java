ba48b144f2dc3b01c4b9117fd68899f0eeb3ba9e
Alejandro <42224066+altamayo02@users.noreply.github.com>
2024-04-01 01:05:10 -0500

    Imports for HttpService

--- src/main/java/edu/prog3/mssecurity/Services/HttpService.java
+++ src/main/java/edu/prog3/mssecurity/Services/HttpService.java
@@ -1,10 +1,20 @@
-package main.java.edu.prog3.mssecurity.Services;
+package edu.prog3.mssecurity.Services;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Service
 public class HttpService {
     @PostMapping
-	public void call(String URI, String requestBody) {   
+	public void call(String URL, String requestBody) {   
 		HttpRequest request = HttpRequest.newBuilder()
-				.uri(URI.create(URI))
+				.uri(URI.create(URL))
 				.method("POST", HttpRequest.BodyPublishers.ofString(requestBody))
 				.build();
 		HttpResponse<String> response = null;
